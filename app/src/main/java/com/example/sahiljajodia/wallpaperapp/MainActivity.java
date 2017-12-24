18b8aad5f5d9bd664b6cb66dbbd2db8a0b60ad65
Sahil Jajodia <sahiljajodia@Sahils-MacBook-Air.local>
2017-12-24 20:26:55 +0530

    Added albums option

--- app/src/main/java/com/example/sahiljajodia/wallpaperapp/MainActivity.java
+++ app/src/main/java/com/example/sahiljajodia/wallpaperapp/MainActivity.java
@@ -1,13 +1,97 @@
 package com.example.sahiljajodia.wallpaperapp;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    Context context = MainActivity.this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        final ArrayList<String> name = new ArrayList<String>();
+        final EditText input = new EditText(getApplicationContext());
+        ListView listView = (ListView) findViewById(R.id.album_list_view);
+        listView.setAdapter(new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1, name));
+
+
+        switch (item.getItemId()) {
+
+            case R.id.action_add_album:
+                final AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("Add Album");
+                builder.setView(input);
+                builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String text = input.getText().toString();
+                        name.add(text);
+
+                    }
+                });
+                builder.setNegativeButton("Cancel", null);
+                builder.show();
+
+
+                }
+
+
+
+//            case R.id.action_add_album:
+//                final Dialog dialog = new Dialog(MainActivity.this);
+//                dialog.setContentView(R.layout.dialog_album_name);
+//                dialog.setTitle("Add Album");
+//
+//                Button button = (Button) findViewById(R.id.add_album_button);
+//                button.setOnClickListener(new View.OnClickListener() {
+//                    @Override
+//                    public void onClick(View view) {
+//
+//                        EditText edit = (EditText) dialog.findViewById(R.id.username);
+//                        String text = edit.getText().toString();
+//                        name.add(text);
+//                    }
+//                });
+//
+//                dialog.show();
+//                ListView listView = (ListView) findViewById(R.id.album_list_view);
+//                listView.setAdapter(new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1, name));
+
+
+                return super.onOptionsItemSelected(item);
+
+
     }
 }
